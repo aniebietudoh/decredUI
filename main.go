c63028e3e6e77f2f4063926c7096d87efc738f8f
Aniebiet Udoh Robert <aniebiet@Sprinthub-Cheetah.local>
2019-09-04 16:10:23 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"image/color"
+	"log"
+
+	"gioui.org/ui"
+	"gioui.org/ui/app"
+	"gioui.org/ui/layout"
+	"gioui.org/ui/measure"
+	"gioui.org/ui/paint"
+	"gioui.org/ui/text"
+	"golang.org/x/image/font/gofont/goregular"
+	"golang.org/x/image/font/sfnt"
+)
+
+func main() {
+	go func() {
+		w := app.NewWindow()
+		if err := loop(w); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	app.Main()
+}
+
+func loop(w *app.Window) error {
+	regular, err := sfnt.Parse(goregular.TTF)
+	if err != nil {
+		panic("failed to load font")
+	}
+	var cfg app.Config
+	var faces measure.Faces
+	grey := color.RGBA{17, 0, 0, 190}
+	face := faces.For(regular, ui.Sp(30))
+	message := "Welcome to \nDecred iOS Wallet"
+	ops := new(ui.Ops)
+	for {
+		e := <-w.Events()
+		switch e := e.(type) {
+		case app.DestroyEvent:
+			return e.Err
+		case app.UpdateEvent:
+			cfg = e.Config
+			faces.Reset(&cfg)
+			cs := layout.RigidConstraints(e.Size)
+			ops.Reset()
+			var material ui.MacroOp
+			material.Record(ops)
+			paint.ColorOp{Color: grey}.Add(ops)
+			material.Stop()
+			text.Label{Material: material, Face: face, Alignment: text.Start, Text: message}.Layout(ops, cs)
+			w.Update(ops)
+		}
+	}
+}
