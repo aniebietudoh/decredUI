005dab2f13d5d36b4a8aa38e2f875a5c1aad0a8b
revealtheweb <aniudohr@gmail.com>
2019-09-27 12:32:32 +0100

    adds canvas element

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"image/color"
+
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/canvas"
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
@@ -13,8 +16,13 @@ func main() {
 	a.Settings().SetTheme(theme.LightTheme())
 	w := a.NewWindow("decred")
 	w.Resize(fyne.NewSize(340, 600))
+
+	text1 := canvas.NewText(" decred", color.RGBA{0x00, 0x00, 0xdd, 0xff})
+	container := fyne.NewContainer(text1)
+
 	w.SetContent(widget.NewVBox(
-		widget.NewLabelWithStyle("decred", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+		container,
+		//widget.NewLabelWithStyle("decred", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
 		widget.NewLabelWithStyle("Welcome to \nDecred iOS Wallet", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
 		layout.NewSpacer(),
 		widget.NewButton("Create new wallet", func() {
